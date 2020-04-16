c686cc88e574c8580644c661ca75920cc7b18a0a
Arthur <arthurrobinsoniii3@gmail.com>
2020-04-15 23:18:17 -0400

    final commit of the night

--- PAM.MVC/Controllers/SearchController.cs
+++ PAM.MVC/Controllers/SearchController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using PAM.DataN;
+using PAM.ServicesN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +12,47 @@ namespace PAM.MVC.Controllers
 {
     public class SearchController : Controller
     {
+        private SearchServices sService = new SearchServices();
+        private MarketDataServices mService = new MarketDataServices();
+        private ApplicationDbContext _db = new ApplicationDbContext();
         // GET: Search
         public ActionResult Index()
         {
+            return View(_db.Searches.ToList());
+        }
+        // GET: Search/Create
+        public ActionResult Create()
+        {
             return View();
         }
+        // POST: Search/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Search search)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                if (sService.HasBeenSearched(search.Stock))
+                {
+
+                    IQueryable<MarketData> dataQueryOne =
+                    from data in _db.MarketDataPoints
+                    where data.Ticker == search.Stock
+                    select data;
+                    return View(dataQueryOne.ToList());
+
+                }
+                await mService.GetMarketData(search.Stock);
+                IQueryable<MarketData> dataQuery =
+                    from data in _db.MarketDataPoints
+                    where data.Ticker == search.Stock
+                    select data;
+                return View(dataQuery.ToList());
+            }
+            return View(search);
+        }
+        
     }
 }
--- PAM.ServicesN/SearchServices.cs
+++ PAM.ServicesN/SearchServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PAM.DataN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace PAM.ServicesN
 {
     public class SearchServices
     {
+        ApplicationDbContext _db = new ApplicationDbContext();
+        public bool HasBeenSearched(string ticker)
+        {
+            IQueryable<MarketData> dataQuery =
+                    from data in _db.MarketDataPoints
+                    where data.Ticker == ticker
+                    select data;
+            if (dataQuery.Count() > 0)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
